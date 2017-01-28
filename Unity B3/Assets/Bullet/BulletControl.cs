905cefc071e2c0702f0c90a0de98e51a3be9ef9d
Ciaranshu <cscshu@outlook.com>
2017-01-28 20:36:29 +0800

    Bullet Initializarion

---
+++ Unity B3/Assets/Bullet/BulletControl.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class BulletControl : MonoBehaviour
+{
+	//the power of bullet
+	public int damage = 20;
+	//the speed of bullet
+	public float speed = 1000.0f;
+
+	void Start(){
+		GetComponent<Rigidbody>().AddForce(transform.forward*speed);
+	}
+}
