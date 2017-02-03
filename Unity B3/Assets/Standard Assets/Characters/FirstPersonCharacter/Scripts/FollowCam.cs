d66a8db7ca94babcf6a80b6c53c54dd6e622a7f6
谢欣言 <495094655@qq.com>
2017-02-03 12:00:09 +0800

    主人公移动逻辑_摄像机追踪_V1
    添加FollowCam.cs, PlayerCtrl.cs和主人公模型，并应用至主人公对象。

---
+++ Unity B3/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FollowCam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+	public Transform targetTr;//主人公对象Transform
+	public float dist = 4.0f;//摄像机与主人公距离
+	public float height=3.0f;//摄像机高度
+	public float dampTrace = 20.0f;//平滑追踪变量
+	private Transform tr;//摄像机Transfprm
+
+	// Use this for initialization
+	void Start () {
+		tr=GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LateUpdate () {
+		tr.position = Vector3.Lerp (tr.position,targetTr.position-(targetTr.forward*dist)+(Vector3.up*height),Time.deltaTime*dampTrace);
+		tr.LookAt (targetTr.position);
+	}
+}
---
+++ Unity B3/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/PlayerCtrl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+
+	private float h = 0.0f;
+	private float v = 0.0f;
+
+	//分配变量
+	private Transform tr;
+	//移动速度变量
+	public float moveSpeed=10.0f;
+	//旋转速度变量
+	public float rotSpeed=100.0f;
+
+	// Use this for initialization
+	void Start () {
+		//向初始部分分配Transform
+		tr=GetComponent<Transform>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		h = Input.GetAxis ("Horizontal");
+		v = Input.GetAxis ("Vertical");
+
+		Debug.Log ("H=" + h.ToString ());
+		Debug.Log ("V=" + v.ToString ());
+
+		Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+		tr.Translate (moveDir.normalized * Time.deltaTime * moveSpeed, Space.Self);
+
+		tr.Rotate (Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis ("Mouse X"));
+	}
+}
