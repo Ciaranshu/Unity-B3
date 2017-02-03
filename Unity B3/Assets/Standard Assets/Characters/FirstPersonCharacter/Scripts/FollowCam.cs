17cd6c0db89c00ae7b133d89c9b72d28839fb98c
Ciaranshu <cscshu@outlook.com>
2017-02-03 21:51:19 +0800

    绑定子弹发射位置及子弹发射轨迹_V1
    1.空格键和鼠标左键控制子弹发射
    2.绑定子弹发射位置至主角代步车灯前
    3.完成子弹发射轨迹的贴图
    4.调整主角跟随摄像机的距离和高度

--- Unity B3/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FollowCam.cs
+++ Unity B3/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FollowCam.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class FollowCam : MonoBehaviour {
 	public Transform targetTr;//主人公对象Transform
-	public float dist = 4.0f;//摄像机与主人公距离
-	public float height=3.0f;//摄像机高度
-	public float dampTrace = 20.0f;//平滑追踪变量
+	public float dist = 2.5f;//摄像机与主人公距离
+	public float height=1.0f;//摄像机高度
+	public float dampTrace =100.0f;//平滑追踪变量
 	private Transform tr;//摄像机Transfprm
 
 	// Use this for initialization
