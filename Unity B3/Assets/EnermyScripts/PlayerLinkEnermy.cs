cdb4fccc89a584fb81c195096677b97c187b4e63
linanqi <linanqichn@163.com>
2017-02-04 18:22:25 +0800

    怪物场景测试_V1
    场景一groundBossTest      Boss测试
    场景二OrdEnermyTest       小怪测试
    
    全在assets第一层文件中

--- Unity B3/Assets/EnermyScripts/PlayerLinkEnermy.cs
+++ Unity B3/Assets/EnermyScripts/PlayerLinkEnermy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCtrl : MonoBehaviour {
+public class PlayerLinkEnermy : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
