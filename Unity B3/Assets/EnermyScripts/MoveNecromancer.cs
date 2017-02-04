ea2b646976c963fe07dbce5520759f446f0f6f37
linanqi <linanqichn@163.com>
2017-02-04 15:41:27 +0800

    小怪移动&技能、Boss移动&技能、陷阱和Readme以及实例_V1
    以Enermy开头的4个文件夹，其中Scenes为各种怪物效果的实例
    ground是行动路径文件夹
    Resources是逻辑中动态加载的Prefabs

---
+++ Unity B3/Assets/EnermyScripts/MoveNecromancer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveNecromancer : EnermyMove {
+
+	private AttackSkill skill;
+
+	// Use this for initialization
+	void Start () {
+		playerObj = GameObject.FindWithTag (EnermyInfo.PlayerTag);//player
+		modelTrns = transform.FindChild (EnermyInfo.tag_NecromancerModel);//model transformation to move without other parts
+
+		skill = 
+			new AttackSkill (
+			transform.FindChild (EnermyInfo.tag_NecromancerAttackCore).gameObject,
+			EnermyInfo.tag_NecromancerAttack);//skill initialization
+
+		attackPlayer (skill);//call the attack
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		move ();
+	}
+
+	protected override void move ()
+	{	//towards player
+		modelTrns.RotateAround(transform.position,Vector3.up,
+			Vector3.Angle(transform.forward,playerObj.transform.position));
+		if (skill.getPS ().isStopped) {
+			//Debug.Log ("攻击暂停？");
+			skill.setLookAt (playerObj.transform.position);
+		}
+	}
+	protected override void attackPlayer (AttackSkill attackSkill)
+	{
+		InvokeRepeating ("emit", EnermyInfo.f_NecromancerAttackStart, EnermyInfo.f_NecromancerAttackStop);
+	}
+
+	private void emit(){
+		skill.getPS ().Play ();
+		Invoke ("stop", EnermyInfo.f_NecromancerAttackStay);
+	}
+	private void stop(){
+		skill.getPS ().Stop ();
+	}
+
+}
