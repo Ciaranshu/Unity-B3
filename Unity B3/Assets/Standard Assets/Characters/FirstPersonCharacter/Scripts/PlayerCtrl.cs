9f3a77815c447abe69bf2651137377a524e2c166
谢欣言 <495094655@qq.com>
2017-02-05 21:41:03 +0800

    死亡复活处理_V1
    完成部分逻辑 部分参数未确定(复活位置)

--- Unity B3/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/PlayerCtrl.cs
+++ Unity B3/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/PlayerCtrl.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour {
 
-	//private float h = 0.0f;
+    //初始化血量
+    public float hp =100f; 
+    //初始化弹药数量
+    public float ammo = 100f;
+    //死亡状态
+    public bool isDead = false;
+    //复活等待时间
+    private float waitingTime=3.0f;
+
 	private float v = 0.0f;
 
 	//分配变量
@@ -23,15 +31,47 @@ public class PlayerCtrl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	//	h = Input.GetAxis ("Horizontal");
+
 		v = Input.GetAxis ("Vertical");
 
 	//	Debug.Log ("H=" + h.ToString ());
 	//	Debug.Log ("V=" + v.ToString ());
 
+        //主人公移动
 		Vector3 moveDir = (Vector3.forward * v) ;
 		tr.Translate (moveDir.normalized * Time.deltaTime * moveSpeed, Space.Self);
 
+        //主人公旋转
 		tr.Rotate (Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis ("Horizontal"));
-	}
+
+        //检测血量
+     // if (hp <= 0) StartCoroutine(this.RespawnPlayer());
+
+    }
+
+    IEnumerator RespawnPlayer()
+    {
+        
+        isDead = true;
+
+        //不可攻击 不被攻击
+
+        //等待复活时间 期间不可移动 
+        moveSpeed = 0;
+        rotSpeed = 0;
+        yield return new WaitForSeconds(waitingTime);
+
+        //重置人物血量
+        hp = 100;
+
+        //重置人物弹药量
+        if (ammo < 100) ammo = 100;
+
+        //在某一位置复活
+        tr.position = new Vector3(0.0f, 0.0f, 0.0f);
+
+        moveSpeed = 10.0f;
+        rotSpeed = 100.0f;
+        isDead = false;
+    }
 }
