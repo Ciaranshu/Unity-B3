bc98399cb89136546943d05d0d4082005cfd6245
谢欣言 <495094655@qq.com>
2017-02-16 00:01:49 +0800

    摄像机追踪_V2
    试了一下改变摄像机的朝向角度用于斜面，可行，但还需要完善一下触发进入斜面的部分；创建了ThirdPersonCharacter-Meterials-AngularSurface这个材料，摩擦系数大，对斜面应用后不会下滑

--- Unity B3/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FollowCam.cs
+++ Unity B3/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FollowCam.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 
 public class FollowCam : MonoBehaviour {
-	public Transform targetTr;//主人公对象Transform
-	public float dist = 2.5f;//摄像机与主人公距离
-	public float height=1.0f;//摄像机高度
-	public float dampTrace =100.0f;//平滑追踪变量
-	private Transform tr;//摄像机Transfprm
+    //public Transform targetTr;//主人公对象Transform
+    //public float dist = 2.5f;//摄像机与主人公距离
+    //public float height = 1.0f;//摄像机高度
+    //public float dampTrace = 100.0f;//平滑追踪变量
+    private Transform tr;//摄像机Transfprm
 
 	// Use this for initialization
 	void Start () {
@@ -17,11 +17,17 @@ public class FollowCam : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+
+
+    }
 
 	void LateUpdate () {
-		tr.position = Vector3.Lerp (tr.position,targetTr.position-(targetTr.forward*dist)+(Vector3.up*height),Time.deltaTime*dampTrace);
-		tr.LookAt (targetTr.position);
+
 	}
 }
+
+//以下注释放置在触发斜坡处，以改变Camera视角
+//Vector3 rotation = tr.localEulerAngles;
+//rotation.x = 9f; // 在这里修改坐标轴的值
+//tr.localEulerAngles = rotation;
